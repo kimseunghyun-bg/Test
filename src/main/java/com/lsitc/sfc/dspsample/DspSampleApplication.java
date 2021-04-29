c2a2e81b6dbb571968c0eab47fa22ac1229c78b0
shkim <shkim@lsitc.com>
2021-04-29 12:19:26 +0900

    초기세팅

---
+++ src/main/java/com/lsitc/sfc/dspsample/DspSampleApplication.java
@@ -0,0 +1,13 @@
+package com.lsitc.sfc.dspsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DspSampleApplication.class, args);
+    }
+
+}
