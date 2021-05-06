1dbcde96c262cb48afe6d57b104999cfd7994f54
shkim <shkim@lsitc.com>
2021-05-06 10:17:37 +0900

    20210506 이훈희 작업

--- frontend/src/layout/DashboardLayout.vue
+++ frontend/src/layout/DashboardLayout.vue
@@ -2,10 +2,11 @@
   <div class="wrapper">
     <side-bar>
       <mobile-menu slot="content"></mobile-menu>
-      <sidebar-link to="/admin/overview">
-        <i class="nc-icon nc-chart-pie-35"></i>
-        <p>Dashboard</p>
+      <sidebar-link v-for='menu in menuList' :key="menu.menuId" :to='menu.url'>
+        <i class="nc-icon" :class="menu.icon"></i>
+        <p>{{menu.menuName}}</p>
       </sidebar-link>
+      <!--
       <sidebar-link to="/admin/user">
         <i class="nc-icon nc-circle-09"></i>
         <p>User Profile</p>
@@ -34,7 +35,7 @@
         <i class="nc-icon nc-bell-55"></i>
         <p>Message</p>
       </sidebar-link>
-
+-->
       <!-- <template slot="bottom-links">
         <sidebar-link class="active"
                       to="/admin/upgrade">
@@ -63,18 +64,114 @@
   import DashboardContent from './Content.vue'
   import MobileMenu from './MobileMenu.vue'
   export default {
+    data(){
+      return {
+        menuList: [
+          {menuId: '1', menuName: 'Dashboard', url: '/admin/overview', icon : 'nc-chart-pie-35'},
+          {menuId: '2', menuName: 'User Profile', url: '/admin/user', icon : 'nc-circle-09'},
+          {menuId: '3', menuName: 'Table list', url: '/admin/table-list', icon : 'nc-notes'},
+          {menuId: '4', menuName: 'Typography', url: '/admin/typography', icon : 'nc-paper-2'},
+          {menuId: '5', menuName: 'Icons', url: '/admin/icons', icon : 'nc-atom'},
+          {menuId: '6', menuName: 'Maps', url: '/admin/maps', icon : 'nc-pin-3'},
+          {menuId: '7', menuName: 'Notifications', url: '/admin/notifications', icon : 'nc-bell-55'}
+        ],
+        routesData: [
+          {
+            path: '/admin',
+            component: resolve => require(['../layout/DashboardLayout.vue'], resolve),
+            redirect: '/admin/overview',
+            children: [
+              {
+                path: '/admin/overview',
+                name: 'Overview',
+                component: resolve => require(['../pages/Overview.vue'], resolve)
+              },
+              {
+                path: '/admin/user',
+                name: 'User',
+                component: resolve => require(['../pages/UserProfile.vue'], resolve)
+              },
+              {
+                path: '/admin/table-list',
+                name: 'Table List',
+                component: resolve => require(['../pages/TableList.vue'], resolve)
+              },
+              {
+                path: '/admin/typography',
+                name: 'Typography',
+                component: resolve => require(['../pages/Typography.vue'], resolve)
+              },
+              {
+                path: '/admin/icons',
+                name: 'Icons',
+                component: resolve => require(['..//pages/Icons.vue'], resolve)
+              },
+              {
+                path: '/admin/maps',
+                name: 'Maps',
+                component: resolve => require(['../pages/Maps.vue'], resolve)
+              },
+              {
+                path: '/admin/notifications',
+                name: 'Notifications',
+                component: resolve => require(['../pages/Notifications.vue'], resolve)
+              }
+            ]
+          }
+        ]
+      }
+    },
     components: {
       TopNavbar,
       ContentFooter,
       DashboardContent,
       MobileMenu
     },
+    created() {
+      const routes = this.routerFormat(this.routesData);
+      this.$router.addRoutes(routes);
+      this.$router.push('/admin/overview');
+    },
     methods: {
       toggleSidebar () {
         if (this.$sidebar.showSidebar) {
           this.$sidebar.displaySidebar(false)
         }
-      }
+      },
+      routerFormat: function rFormat(routers) {
+        //console.log("inside routerFormat");
+        if (!(routers instanceof Array)) {
+          return false;
+        }
+        let fmRouters = [];
+        //this.$axios.post("/api/menu").then(res => {
+          routers.forEach(router => {
+            let { path = "/404", component = "Error", name, children } = router;
+            path = path || "";
+            component = component || "Error";
+            if (children && children instanceof Array) {
+              children = rFormat(children);
+            }
+            //console.log("component", component);
+            let fmRouter = {
+              path: path,
+              component: component, // Import about page in the top
+              //component: () => import(`./${name}.vue`).then(m => m),
+              name: name,
+              children: children
+            };
+            console.log(fmRouter);
+            fmRouters.push(fmRouter);
+          });
+          /*
+        }).catch(err => {
+          console.log(err);
+        })
+        */
+        //console.log("fmRouters", fmRouters);
+        return fmRouters;
+      },
+      addRoutes() {}
     }
   }
 
