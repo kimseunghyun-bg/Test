f207cc478e7d855a2dcc00644fe61b945859d033
shkim <shkim@lsitc.com>
2021-05-14 14:32:15 +0900

    [추가] Web 다국어 처리를 위해 LocaleResolver 등록

--- dsp_portal_web/src/main/java/com/lsitc/sfc/dspsample/dummy/controller/TestDummyController.java
+++ dsp_portal_web/src/main/java/com/lsitc/sfc/dspsample/dummy/controller/TestDummyController.java
@@ -1,6 +1,7 @@
 package com.lsitc.sfc.dspsample.dummy.controller;
 
 import com.lsitc.sfc.core.service.PropertyService;
+import com.lsitc.sfc.core.util.MessageUtils;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,7 +10,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Locale;
+import java.util.Map;
 
 @RestController
 public class TestDummyController {
@@ -28,11 +29,12 @@ public class TestDummyController {
 	}
 
 	@GetMapping("/api/message")
-	public void getMessage(Locale locale) {
-		String key = "title";
-		String message = messageSource.getMessage(key, null, Locale.ENGLISH);
-		System.out.println(message);
-		System.out.println(propertyService.getString("test"));
+	public Map<String, String> getMessage() {
+		Map<String, String> allMessage = MessageUtils.getAllMessage();
+		for (String name : allMessage.keySet()) {
+			System.out.println(name + ": " + allMessage.get(name));
+		}
+		return allMessage;
 	}
 
 }
