fc3fcaa4794702719685d21e1251b810ea57a9db
shkim <shkim@lsitc.com>
2021-05-14 11:00:45 +0900

    [수정] Getter, Setter 변경

--- dsp_portal_web/src/main/java/com/lsitc/sfc/dspsample/dummy/controller/TestDummyController.java
+++ dsp_portal_web/src/main/java/com/lsitc/sfc/dspsample/dummy/controller/TestDummyController.java
@@ -1,6 +1,9 @@
 package com.lsitc.sfc.dspsample.dummy.controller;
 
 import com.lsitc.sfc.core.service.PropertyService;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,17 +37,20 @@ public class TestDummyController {
 
 }
 
+@Getter
+@Setter
+@ToString
 class TestData {
-    private String company;
-    private String username;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String country;
-    private String postalCode;
-    private String aboutMe;
+	private String company;
+	private String username;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String country;
+	private String postalCode;
+	private String aboutMe;
 
 	public TestData() {
 		super();
@@ -59,87 +65,4 @@ class TestData {
 		this.postalCode = "0000";
 		this.aboutMe = "이훈희입니다";
 	}
-
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getPostalCode() {
-		return postalCode;
-	}
-
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
-
-	public String getAboutMe() {
-		return aboutMe;
-	}
-
-	public void setAboutMe(String aboutMe) {
-		this.aboutMe = aboutMe;
-	}
-	
-	
-
 }
